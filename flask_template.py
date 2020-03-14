4774a7442ad1f8391b978ad4bc29332ac57d7a5f
Armaan Goel <armaan.goel@berkeley.edu>
2020-03-13 17:38:07 -0700

    autorefresh

--- flask_template.py
+++ flask_template.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 from time import sleep
 app = Flask(__name__)
 i = 1
+
 @app.route("/")
 def index():
+    return render_template("index.html")
+    
+@app.route("/animals", methods=['GET'])
+def get_animals():
     global i 
     l = ["cat", "dog", "pikachu"]
     st = l[i%len(l)]
     i+=1
-    return render_template("index.html", animal=st)
-    
+
+    return jsonify(st)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
