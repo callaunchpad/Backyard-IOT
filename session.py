fcc2bd31a40a5e14b317abb02183bc985273a80d
Max Emerling <memerling@berkeley.edu>
2020-03-26 15:36:23 -0700

    using data generator

--- session.py
+++ session.py
@@ -1,53 +1,65 @@
 import tensorflow as tf
-import keras
-from keras import applications
+
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import LearningRateScheduler
-import numpy as np
-from sklearn.model_selection import train_test_split
+from keras.applications.resnet50 import ResNet50
+from keras.layers import GlobalAveragePooling2D, Dropout, Dense
+from keras.optimizers import Adam
+from keras.models import Model
+
+
+from collections import Counter
+
+DIR='data'
+DIM=(512, 384)
+INPUT_SHAPE=(DIM[1], DIM[0], 3)
+
+DROPOUT = 0.3
+
+BATCH_SIZE=24
+NUM_EPOCHS=500
+INIT_LR = 0.001
+
+datagen = ImageDataGenerator(rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=1/12)
+
+train_generator = datagen.flow_from_directory(
+    DIR,
+    target_size=DIM,
+    batch_size=BATCH_SIZE,
+    subset='training')
+
+validation_generator = datagen.flow_from_directory(
+    DIR,
+    target_size=DIM,
+    batch_size=BATCH_SIZE,
+    subset='validation')
+
+counter = Counter(train_generator.classes)                        
+max_val = float(max(values))       
+class_weights = {class_id : max_val/num_images for class_id, num_images in counter.items()}
+
+base_model = ResNet50(weights=None, include_top=False, input_shape=INPUT_SHAPE)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dropout(DROPOUT)(x)
+predictions = Dense(len(class_weights), activation= 'softmax')(x)
 
 strategy = tf.distribute.MirroredStrategy()
-
-images = np.load('images.npy')
-labels = np.load('labels.npy')
-print(images.shape)
-INPUT_SHAPE = images[0].shape
-
-X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.1)
-del images
-del labels
-
-y_lst = list(np.argmax(y_train, axis=1))
-unique, counts = np.unique(np.argmax(y_train, axis=1), return_counts=True)
-class_weights = {unique[i]:counts[i] for i in range(len(unique))}
-for i in range(NUM_CLASSES):
-    if i not in class_weights:
-        class_weights[i] = 0
-        
-base_model = applications.resnet50.ResNet50(weights=None, include_top=False, input_shape=INPUT_SHAPE)
-
-DROPOUT = 0.2
-x = base_model.output
-x = keras.layers.GlobalAveragePooling2D()(x)
-x = keras.layers.Dropout(DROPOUT)(x)
-predictions = keras.layers.Dense(NUM_CLASSES, activation= 'softmax')(x)
-
-from keras.optimizers import Adam
-
-adam = Adam(lr=0.005)
-
 with strategy.scope():
-    model = keras.models.Model(inputs = base_model.input, outputs = predictions)
-    model.compile(optimizer= adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    model = Model(inputs = base_model.input, outputs = predictions)
+    model.compile(optimizer=Adam(lr=INIT_LR), loss='categorical_crossentropy', metrics=['accuracy'])
     
-NUM_EPOCHS = 100
-BATCH_SIZE = 10
-
-model.fit(X_train, y_train, epochs = NUM_EPOCHS, batch_size = BATCH_SIZE,
-          validation_data=(X_test, y_test), class_weight=class_weights)
-
-print('\n# Evaluate on test data')
-results = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
-print('test loss, test acc:', results)
-
-model.save("model.h5")
+    model.fit_generator(
+    train_generator,
+    steps_per_epoch = train_generator.samples // BATCH_SIZE,
+    validation_data = validation_generator,
+    validation_steps = validation_generator.samples // BATCH_SIZE,
+    epochs=NUM_EPOCHS,
+    class_weight=class_weights,
+    shuffle=True
+)
+    
+model.save('model.h5')
