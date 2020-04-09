83b9c091e1a848b58d6a2e228e0872b36b36ea57
Max Emerling <memerling@berkeley.edu>
2020-04-09 11:14:13 -0700

    eccv mobilenet training pipeline

--- MobileNet/eccv_train.py
+++ MobileNet/eccv_train.py
@@ -8,31 +8,31 @@ from tensorflow.keras.models import Sequential
 
 from collections import Counter
 
-DIR='data'
-WIDTH, HEIGHT, CHANNELS = (512, 384, 3)
-VALIDATION_SPLIT=4800/57864
+DIR='../Data/eccv'
+WIDTH, HEIGHT, CHANNELS = (300, 300, 3)
+VALIDATION_SPLIT=0.1
 
-DROPOUT = 0.3
-
-BATCH_SIZE=24
-NUM_EPOCHS=500
+BATCH_SIZE=32
+NUM_EPOCHS=10
 INIT_LR = 0.001
+CLASSES=10
 
 datagen = ImageDataGenerator(rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    validation_split=VALIDATION_SPLIT)
+    validation_split=0.1)
 
 train_generator = datagen.flow_from_directory(
-    DIR,
+    PATH,
     target_size=(HEIGHT, WIDTH),
+    color_mode='rgb',
+    class_mode='categorical',
     batch_size=BATCH_SIZE,
     subset='training')
 
 validation_generator = datagen.flow_from_directory(
-    DIR,
+    PATH,
     target_size=(HEIGHT, WIDTH),
+    color_mode='rgb',
+    class_mode='categorical',
     batch_size=BATCH_SIZE,
     subset='validation')
 
@@ -48,20 +48,25 @@ predictions = Dense(len(class_weights), activation= 'softmax')(x)
 
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
-    model = Model(inputs = base_model.input, outputs = predictions)
-    model.compile(optimizer=Adam(lr=INIT_LR), loss='categorical_crossentropy', metrics=['accuracy'])
-    
-model.fit_generator(
+    base_model = MobileNetV2(input_shape=(HEIGHT, WIDTH, 3),
+                             include_top=False,
+                             weights='imagenet')
+    base_model.trainable = False
+
+    pool = GlobalAveragePooling2D()
+    predictions = Dense(CLASSES, activation='softmax')
+    model = Sequential([base_model, pool, predictions])
+    model.compile(optimizer=RMSprop(lr=INIT_LR),
+                  loss=CategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+model.fit(
     train_generator,
     steps_per_epoch = train_generator.samples // BATCH_SIZE,
-    validation_data = validation_generator,
-    validation_steps = validation_generator.samples // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // BATCH_SIZE,
     epochs=NUM_EPOCHS,
-    class_weight=class_weights,
-    shuffle=True
+    class_weight=class_weights
 )
 
-model.evaluate_generator(generator=validation_generator,
-steps=validation_generator.samples // BATCH_SIZE)
-    
 model.save('model.h5')
