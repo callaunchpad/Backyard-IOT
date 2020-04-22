97f241ca93ccda010c312cf3d1378c3e72845435
Max Emerling <memerling@berkeley.edu>
2020-04-22 14:28:28 -0700

    redid squeezenet

--- SqueezeNet/keras_squeezenet.py
+++ SqueezeNet/keras_squeezenet.py
@@ -1,12 +1,5 @@
-"""
-Adapted from https://github.com/rcmalli/keras-squeezenet/blob/master/keras_squeezenet/squeezenet.py
-"""
-
 from keras.models import Model
-from keras.layers import Input, Convolution2D, Activation, MaxPooling2D, GlobalAveragePooling2D, Dropout, concatenate, add
-from keras.utils import get_file
-
-WEIGHTS_PATH_NO_TOP = "https://github.com/rcmalli/keras-squeezenet/releases/download/v1.0/squeezenet_weights_tf_dim_ordering_tf_kernels_notop.h5"
+from keras.layers import Input, Convolution2D, Dropout, Activation, MaxPooling2D, GlobalAveragePooling2D, concatenate, add
 
 def fire_module(x, fire_id, squeeze=16, expand=64):
     fire_id = 'fire' + str(fire_id) + '/'
@@ -29,27 +22,20 @@ def simple_add(from_layer, to_layer):
 def channels(layer):
     return layer.shape[3]
 
-def complex_add(complex_id, from_layer, to_layer):
-    complex_id = 'complex' + str(complex_id)
-    from_layer = Convolution2D(channels(to_layer), (1, 1), padding='same', name=complex_id)(from_layer)
-    from_layer = Activation('relu', name='relu_' + complex_id)(from_layer)
+def complex_add(from_layer, to_layer):
+    from_layer = Convolution2D(channels(to_layer), (1, 1), padding='same')(from_layer)
+    from_layer = Activation('relu')(from_layer)
     return simple_add(from_layer, to_layer)
 
-"""
-Note: the simple and complex bypass connections feed into each other unless separated by a pooling layer.
-"""
-def SqueezeNet(input_shape, weights='imagenet', bypass=None):
-    
-    if weights not in {None, 'imagenet', 'base_model'}:
-        raise ValueError('Unknown `weights` argument')
-    
-    if bypass == 'simple':
+def SqueezeNet(input_shape, classes=1000, bypass=None):
+       
+    if bypass == 'complex':
+        complex = simple = True
+    elif bypass == 'simple':
         simple = True
         complex = False
-    elif bypass == 'complex':
-        simple = complex = True
     elif bypass == None:
-        simple = complex = False
+        complex = simple = False
     else:
         raise ValueError('Unknown `bypass` argument')
     
@@ -57,46 +43,43 @@ def SqueezeNet(input_shape, weights='imagenet', bypass=None):
     
     x = Convolution2D(64, (3, 3), strides=(2, 2), padding='valid', name='conv1')(inputs)
     x = Activation('relu', name='relu_conv1')(x)
-    c = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
+    c = x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
 
-    s = fire_module(c, fire_id=2, squeeze=16, expand=64)
+    s = x = fire_module(x, fire_id=2, squeeze=16, expand=64)
     if complex:
-        s = complex_add(1, c, s)
-    c = fire_module(s, fire_id=3, squeeze=16, expand=64)
+        x = complex_add(c, x) #complex conv1 -> fire3
+    x = fire_module(x, fire_id=3, squeeze=16, expand=64)
     if simple:
-        c = simple_add(s, c)
-    x = fire_module(c, fire_id=4, squeeze=32, expand=128)
-    if complex:
-        x = complex_add(2, c, x)
-    s = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
+        x = simple_add(s, x) #simple fire2 -> fire4
+    c = x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
     
-    c = fire_module(s, fire_id=5, squeeze=32, expand=128)
-    if simple:
-        c = simple_add(s, c)
-    s = fire_module(c, fire_id=6, squeeze=48, expand=192)
+    s = x = fire_module(x, fire_id=4, squeeze=32, expand=128)
     if complex:
-        s = complex_add(3, c, s)
-    c = fire_module(s, fire_id=7, squeeze=48, expand=192)
+        x = complex_add(c, x) #complex fire3 -> fire5
+    x = fire_module(x, fire_id=5, squeeze=32, expand=128)
     if simple:
-        c = simple_add(s, c)
-    x = fire_module(c, fire_id=8, squeeze=64, expand=256)
-    if complex:
-        x = complex_add(4, c, x)
-    s = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
+        x = simple_add(s, x) #simple fire4 -> fire6
+    c = x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
     
-    x = fire_module(s, fire_id=9, squeeze=64, expand=256)
+    s = x = fire_module(x, fire_id=6, squeeze=48, expand=192)
+    if complex:
+        x = complex_add(c, x) #complex fire5 -> fire7
+    c = x = fire_module(x, fire_id=7, squeeze=48, expand=192)
     if simple:
-        x = simple_add(s, x)
+        x = simple_add(s, x) #simple fire6 -> fire8
+    s = x = fire_module(x, fire_id=8, squeeze=64, expand=256)
+    if complex:
+        x = complex_add(c, x) #complex fire7 -> fire9
+    x = fire_module(x, fire_id=9, squeeze=64, expand=256)
+    if simple:
+        x = simple_add(s, x) #simple fire8 -> conv10
+    
+    x = Dropout(0.5, name='drop9')(x)
+    x = Convolution2D(classes, (1, 1), padding='valid', name='conv10')(x)
+    x = Activation('relu', name='relu_conv10')(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Activation('softmax', name='loss')(x)
 
     model = Model(inputs, x, name='squeezenet')
-    
-    if weights == 'base_model':
-        weights_path = 'base_model.h5'
-    elif weights == 'imagenet':
-        weights_path = get_file('squeezenet_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                    WEIGHTS_PATH_NO_TOP,
-                                    cache_subdir='models')
-        model.load_weights(weights_path)
-    
     
     return model
--- SqueezeNet/train.py
+++ SqueezeNet/train.py
@@ -10,115 +10,10 @@ NUM_EPOCHS=35
 PATH = '../Data/animals10/raw-img/'
 
 import tensorflow as tf
-
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Convolution2D, Activation, MaxPooling2D, GlobalAveragePooling2D, Dropout, Dense, concatenate, add
-from tensorflow.keras.utils import get_file
-from tensorflow.keras.optimizers import Adam, RMSprop
-from tensorflow.keras.losses import CategoricalCrossentropy
-
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras_squeezenet import SqueezeNet
 from collections import Counter
-
-#import keras.backend as K
-#K.set_floatx('float16')
-#K.set_epsilon(1e-4) #default is 1e-7
-
-def fire_module(x, fire_id, squeeze=16, expand=64):
-    fire_id = 'fire' + str(fire_id) + '/'
-    x = Convolution2D(squeeze, (1, 1), padding='valid', name=fire_id + 'squeeze1x1')(x)
-    x = Activation('relu', name=fire_id + 'relu_squeeze1x1')(x)
-    
-    left = Convolution2D(expand, (1, 1), padding='valid', name=fire_id + 'expand1x1')(x)
-    left = Activation('relu', name=fire_id + 'relu_expand1x1')(left)
- 
-    right = Convolution2D(expand, (3, 3), padding='same', name=fire_id + 'expand3x3')(x)
-    right = Activation('relu', name=fire_id + 'relu_expand3x3')(right)
- 
-    x = concatenate([left, right], axis=3, name=fire_id + 'concat')
-    
-    return x
-
-def simple_add(from_layer, to_layer):
-    return add([from_layer, to_layer])
-
-def channels(layer):
-    return layer.shape[3]
-
-def complex_add(complex_id, from_layer, to_layer):
-    complex_id = 'complex' + str(complex_id)
-    from_layer = Convolution2D(channels(to_layer), (1, 1), padding='same', name=complex_id)(from_layer)
-    from_layer = Activation('relu', name='relu_' + complex_id)(from_layer)
-    return simple_add(from_layer, to_layer)
-
-
-"""
-Note: the simple and complex bypass connections feed into each other unless separated by a pooling layer.
-"""
-def SqueezeNet(input_shape, weights=None, bypass=None):
-    
-    if weights not in {None, 'base_model'}:
-        raise ValueError('Unknown `weights` argument')
-    
-    if bypass == 'simple':
-        simple = True
-        complex = False
-    elif bypass == 'complex':
-        simple = complex = True
-    elif bypass == None:
-        simple = complex = False
-    else:
-        raise ValueError('Unknown `bypass` argument')
-    
-    inputs = Input(shape=input_shape, name='input')
-    
-    x = Convolution2D(64, 3, strides=2, padding='valid', name='conv1')(inputs)
-    x = Activation('relu', name='relu_conv1')(x)
-    x = c = MaxPooling2D(pool_size=3, strides=2, name='maxpool1')(x)
-
-    x = s = fire_module(x, fire_id=2, squeeze=16, expand=64)
-    if complex:
-        x = complex_add(1, c, x)
-    x = c = fire_module(x, fire_id=3, squeeze=16, expand=64)
-    if simple:
-        x = simple_add(s, x)
-    x = fire_module(x, fire_id=4, squeeze=32, expand=128)
-    if complex:
-        x = complex_add(2, c, x)
-    x = s = MaxPooling2D(pool_size=3, strides=2, name='maxpool4')(x)
-    
-    x = c = fire_module(x, fire_id=5, squeeze=32, expand=128)
-    if simple:
-        x = simple_add(s, x)
-    x = s = fire_module(x, fire_id=6, squeeze=48, expand=192)
-    if complex:
-        x = complex_add(3, c, x)
-    x = c = fire_module(x, fire_id=7, squeeze=48, expand=192)
-    if simple:
-        x = simple_add(s, x)
-    x = fire_module(x, fire_id=8, squeeze=64, expand=256)
-    if complex:
-        x = complex_add(4, c, x)
-    x = s = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='maxpool8')(x)
-    
-    x = fire_module(s, fire_id=9, squeeze=64, expand=256)
-    if simple:
-        x = simple_add(s, x)
-        
-    #x = Dropout(DROPOUT, name='drop9')(x)
-    
-    #x = Convolution2D(CLASSES, 1, strides=1, name='conv10')(x)
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(CLASSES)(x)
-    predictions = Activation('softmax', name='predictions')(x)
-    
-    model = Model(inputs, predictions, name='squeezenet')
-    
-    if weights == 'base_model':
-        weights_path = 'base_model.h5'
-    
-    return model
 
 datagen = ImageDataGenerator(rescale=1./255,
     validation_split=0.1)
@@ -145,9 +40,9 @@ class_weights = {class_id : max_val/num_images for class_id, num_images in count
 
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
-    model = SqueezeNet(input_shape=(HEIGHT, WIDTH, 3), weights=None, bypass='simple')
+    model = SqueezeNet(input_shape=(HEIGHT, WIDTH, 3), bypass='simple')
     model.compile(optimizer=Adam(lr=INIT_LR),
-                  loss=CategoricalCrossentropy(from_logits=True),
+                  loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
 history = model.fit(
