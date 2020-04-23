ba3af5243d39a4589a3dcec5cf028c2ce13aaf5e
Max Emerling <memerling@berkeley.edu>
2020-04-22 17:24:20 -0700

    fd mobilenet

---
+++ FD-MobileNet/keras_fdmobilenet.py
@@ -0,0 +1,45 @@
+from tensorflow.keras.layers import Input, Dense, ReLU, Conv2D, SeparableConv2D, BatchNormalization, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+
+def FDMobileNet(input_shape=(224, 224, 3), classes=10, alpha=1):
+    
+    
+    """Helpful custom keras blocks"""
+    
+    def ConvBlock(layer, filters, kernel_size=3, strides=1, **kwargs):
+        def block(x):
+            x = layer(filters, kernel_size, strides, padding='same', use_bias=False, **kwargs)(x)
+            x = BatchNormalization()(x)
+            x = ReLU()(x)
+            return x
+        return block
+
+    SeparableBlock = lambda filters, strides=1: ConvBlock(SeparableConv2D, filters, strides=strides, depth_multiplier=alpha)
+    
+    
+    """Build the model"""
+    
+    inputs = Input(shape=input_shape)
+    
+    x = ConvBlock(Conv2D, filters=32, strides=2)(inputs)
+    
+    x = SeparableBlock(filters=64, strides=2)(x)
+    
+    x = SeparableBlock(filters=128, strides=2)(x)
+    x = SeparableBlock(filters=128)(x)
+    
+    x = SeparableBlock(filters=256, strides=2)(x)
+    x = SeparableBlock(filters=256)(x)
+    
+    x = SeparableBlock(filters=512, strides=2)(x)
+    for i in range(4):
+        x = SeparableBlock(filters=512)(x)
+    x = SeparableBlock(filters=1024)(x)
+    
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(classes, activation='softmax')(x)
+    
+    model = Model(inputs, x, name='FD-MobileNet')
+    
+    
+    return model
---
+++ FD-MobileNet/train.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from keras_fdmobilenet import FDMobileNet
+from collections import Counter
+
+BATCH_SIZE=256
+NUM_EPOCHS=30
+INIT_LR=0.001
+
+WIDTH, HEIGHT = (224, 224)
+CLASSES=10
+PATH = '../Data/animals10/raw-img/'
+
+datagen = ImageDataGenerator(rescale=1./255,
+    validation_split=0.1)
+
+train_generator = datagen.flow_from_directory(
+    PATH,
+    target_size=(HEIGHT, WIDTH),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=BATCH_SIZE,
+    subset='training')
+
+validation_generator = datagen.flow_from_directory(
+    PATH,
+    target_size=(HEIGHT, WIDTH),
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=BATCH_SIZE,
+    subset='validation')
+
+counter = Counter(train_generator.classes)                        
+max_val = float(max(counter.values()))       
+class_weights = {class_id : max_val/num_images for class_id, num_images in counter.items()}
+
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
+    model = FDMobileNet()
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+model.save('model.h5')
