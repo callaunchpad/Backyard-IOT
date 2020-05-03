56dd79c4621b37f1ae9dae978bbf140993d672dd
Max Emerling <memerling@berkeley.edu>
2020-05-02 19:11:08 -0700

    iwildcam train fdmobilenet

--- FD-MobileNet/train.py
+++ FD-MobileNet/train.py
@@ -5,51 +5,84 @@ from keras_fdmobilenet import FDMobileNet
 from collections import Counter
 import os
 import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from prettytable import PrettyTable
 
-BATCH_SIZE=16
-NUM_EPOCHS=12
-INIT_LR=1e-5
-STEP=2
-RATE=0.9
+BATCH_SIZE=128
+NUM_EPOCHS=1
+INIT_LR=1e-4
+STEP=10
+RATE=0.8
 
-WIDTH, HEIGHT = (224, 224)
-CLASSES=10
-PATH = '../Data/animals10/raw-img/'
+IMAGES = '../Data/iwildcam/images'
+LABELS = '../Data/iwildcam/labels.csv'
+WIDTH, HEIGHT = (512, 374)
 
-datagen = ImageDataGenerator(rescale=1./255,
-    validation_split=0.1)
 
-train_generator = datagen.flow_from_directory(
-    PATH,
+df = pd.read_csv(LABELS)
+CLASSES=len(df.label.unique())
+
+train_df, test_df = train_test_split(df, test_size=0.1)
+train_df, val_df = train_test_split(train_df, test_size=0.1)
+
+del df
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1/.255,
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1)
+test_datagen = ImageDataGenerator(
+    rescale=1/.255)
+
+train_generator = train_datagen.flow_from_dataframe(
+    dataframe=train_df,
+    directory=IMAGES,
     target_size=(HEIGHT, WIDTH),
+    x_col='file_name',
+    y_col='label',
     color_mode='rgb',
     class_mode='categorical',
     batch_size=BATCH_SIZE,
-    subset='training')
-#     rotation_range=10
-#     width_shift_range=0.1
-#     height_shift_range=0.1
-#     zoom_range=0.1,
+)
 
-validation_generator = datagen.flow_from_directory(
-    PATH,
+test_generator = test_datagen.flow_from_dataframe(
+    dataframe=test_df,
+    directory=IMAGES,
     target_size=(HEIGHT, WIDTH),
+    x_col='file_name',
+    y_col='label',
     color_mode='rgb',
     class_mode='categorical',
     batch_size=BATCH_SIZE,
-    subset='validation')
+)
 
-counter = Counter(train_generator.classes)                        
-max_val = float(max(counter.values()))       
+val_generator = test_datagen.flow_from_dataframe(
+    dataframe=val_df,
+    directory=IMAGES,
+    target_size=(HEIGHT, WIDTH),
+    x_col='file_name',
+    y_col='label',
+    color_mode='rgb',
+    class_mode='categorical',
+    batch_size=BATCH_SIZE,
+)
+
+
+#Calculate class weights
+counter = Counter(train_generator.classes)
+max_val = float(max(counter.values()))
 class_weights = {class_id : max_val/num_images for class_id, num_images in counter.items()}
 
 #Multiplies learning rate by RATE every STEP epochs, starting at INIT_LR
 scheduler = lambda epoch: INIT_LR * (RATE ** (epoch // STEP))
 callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
-strategy = tf.distribute.MirroredStrategy()
 
+strategy = tf.distribute.MirroredStrategy()
 def train_and_save(alpha):
     with strategy.scope():
         model = FDMobileNet(alpha=alpha)
@@ -60,10 +93,15 @@ def train_and_save(alpha):
     history = model.fit(
         train_generator,
         steps_per_epoch = train_generator.samples // BATCH_SIZE,
-        validation_data=validation_generator,
-        validation_steps=validation_generator.samples // BATCH_SIZE,
+        validation_data=val_generator,
+        validation_steps=val_generator.samples // BATCH_SIZE,
         epochs=NUM_EPOCHS,
         class_weight=class_weights, callbacks=[callback]
+    )
+    
+    evaluation = model.evaluate(
+        test_generator,
+        steps = test_generator.samples // BATCH_SIZE
     )
 
     RESULTS='results'
@@ -78,9 +116,11 @@ def train_and_save(alpha):
     table = PrettyTable(headers)
     for i in range(NUM_EPOCHS):
         table.add_row([i+1] + [history.history[header][i] for header in headers[1:]])
-    with open(os.path.join(RESULTS, 'table_{}x.txt'.format(alpha)), 'wb') as f:
-        pickle.dump(str(table), f)
+    with open(os.path.join(RESULTS, 'table_{}x.txt'.format(alpha)), 'w') as f:
+        f.write(str(table))
+        f.write('\n\nTest Results:')
+        f.write('loss:', evaluation[0])
+        f.write('accuracy:', evaluation[1])
+
 
 train_and_save(1)
-train_and_save(0.5)
-train_and_save(0.25)
