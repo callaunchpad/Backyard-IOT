42e0eaafeb9bf6bdcb627bff8247e763e8da1a17
Max Emerling <memerling@berkeley.edu>
2020-05-03 13:12:29 -0700

    better data

--- FD-MobileNet/train.py
+++ FD-MobileNet/train.py
@@ -12,13 +12,13 @@ from prettytable import PrettyTable
 
 BATCH_SIZE=256
 NUM_EPOCHS=50
-INIT_LR=1e-4
+INIT_LR=1e-5
 STEP=10
 RATE=0.8
 
 IMAGES = '../Data/iwildcam/images'
-LABELS = '../Data/iwildcam/labels.csv'
-WIDTH, HEIGHT = (256, 187)
+LABELS = 'train.csv'
+WIDTH, HEIGHT = (512, 374)
 
 
 RESULTS='results'
@@ -28,26 +28,16 @@ if not os.path.exists(RESULTS):
 df = pd.read_csv(LABELS)
 CLASSES=list(df.label.unique())
 
-df, discard = train_test_split(df, train_size=0.2)
-train_df, test_df = train_test_split(df, test_size=0.1)
 train_df, val_df = train_test_split(train_df, test_size=0.1)
 train_df.to_csv(os.path.join(RESULTS, 'train.csv'), index=False)
 val_df.to_csv(os.path.join(RESULTS, 'val.csv'), index=False)
-test_df.to_csv(os.path.join(RESULTS, 'test.csv'), index=False)
 
 del df, discard
 
-
-train_datagen = ImageDataGenerator(
-    rescale=1/.255,
-    rotation_range=10,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    zoom_range=0.1)
-test_datagen = ImageDataGenerator(
+datagen = ImageDataGenerator(
     rescale=1/.255)
 
-train_generator = train_datagen.flow_from_dataframe(
+train_generator = datagen.flow_from_dataframe(
     classes=CLASSES,
     dataframe=train_df,
     directory=IMAGES,
@@ -59,19 +49,7 @@ train_generator = train_datagen.flow_from_dataframe(
     batch_size=BATCH_SIZE,
 )
 
-test_generator = test_datagen.flow_from_dataframe(
-    classes=CLASSES,
-    dataframe=test_df,
-    directory=IMAGES,
-    target_size=(HEIGHT, WIDTH),
-    x_col='file_name',
-    y_col='label',
-    color_mode='rgb',
-    class_mode='categorical',
-    batch_size=BATCH_SIZE,
-)
-
-val_generator = test_datagen.flow_from_dataframe(
+val_generator = datagen.flow_from_dataframe(
     classes=CLASSES,
     dataframe=val_df,
     directory=IMAGES,
@@ -116,11 +94,6 @@ history = model.fit(
 
 model.save(os.path.join(RESULTS, 'fdmobilenet_{}x.h5'.format(alpha)))
 
-evaluation = model.evaluate(
-    test_generator,
-    steps = test_generator.samples // BATCH_SIZE
-)
-
 with open(os.path.join(RESULTS, 'history_{}x.pkl'.format(alpha)), 'wb') as f:
     pickle.dump(history.history, f)
     
@@ -130,6 +103,3 @@ for i in range(NUM_EPOCHS):
     table.add_row([i+1] + [history.history[header][i] for header in headers[1:]])
 with open(os.path.join(RESULTS, 'table_{}x.txt'.format(alpha)), 'w') as f:
     f.write(str(table))
-    f.write('\n\nTest Results:')
-    f.write('loss:', evaluation[0])
-    f.write('accuracy:', evaluation[1])
