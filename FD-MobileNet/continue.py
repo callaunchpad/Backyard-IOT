b82a233c7df38dc2859723d1040b2ac26e2c9857
Max Emerling <memerling@berkeley.edu>
2020-05-05 20:20:53 -0700

    moved stuff from common

--- FD-MobileNet/continue.py
+++ FD-MobileNet/continue.py
@@ -7,4 +7,23 @@ with strategy.scope():
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-save()
+history = model.fit(
+    train_generator,
+    steps_per_epoch = train_generator.samples // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // BATCH_SIZE,
+    epochs=NUM_EPOCHS,
+    class_weight=class_weights, callbacks=callbacks
+)
+
+model.save( 'model.h5')
+
+with open('history.pkl', 'wb') as f:
+    pickle.dump(history.history, f)
+    
+headers=['epoch', 'accuracy', 'val_accuracy', 'loss', 'val_loss', 'lr']
+table = PrettyTable(headers)
+for i in range(NUM_EPOCHS):
+    table.add_row([i+1] + [history.history[header][i] for header in headers[1:]])
+with open('table.txt', 'w') as f:
+    f.write(str(table))
